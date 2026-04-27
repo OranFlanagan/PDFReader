3be485cb5c25f4f825af317f51bd2892c156e0a3
OranFlanagan <G00428429@atu.ie>
2026-04-27 14:34:04 +0100

    Add PDFReaderTest class for PDF generation testing

---
+++ Main/src/test/PDFReaderTest.java
@@ -0,0 +1,33 @@
+package com.test;
+
+public class PDFReaderTest 
+{
+    @TestMethodOrder(MethodOrderer.OderAnnotation.class)
+    class PDFReaderTest
+    {
+        @TempDir static Path tempDir;
+
+        static File makePdf(String text, int pages) throws Exception
+        {
+            File f = Files.createTempFile(tempDir, "test-", ".pdf").toFile();
+            try(PDDocument doc = new PDDocument())
+            {
+                for(int p = 0; p < pages; p++)
+                {
+                    PDPage page = new PDPage(PDRectangle.A4);
+                    doc.addPage(page);
+                    try(PDPageContentStream cs = new PDPageContentStream(doc, page))
+                    {
+                        cs.beginText();
+                        cs.setFont(new PDType1Font(Standard14Fonts.FontName.HELVETICA), 12);
+                        cs.newLineAtOffset(50, 700 - p * 20);
+                        cs.showText(text + (pages > 1 ? " page " + (p + 1) : ""));
+                        cs.endText();
+                    }
+                }
+                doc.save(f);
+            }
+            return f;
+        }
+    }
+}
